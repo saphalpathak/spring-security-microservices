92d63e02f69a9941c96082b3e89158fe9898613b
saphalpathak <saphalpathak3@gmail.com>
2023-09-22 23:56:56 +0545

    1. Eureka server configuration 2. Loader balancer configuration in auth and blog servie

--- blog-service/src/main/java/com/saphal/blogservice/BlogServiceApplication.java
+++ blog-service/src/main/java/com/saphal/blogservice/BlogServiceApplication.java
@@ -2,8 +2,10 @@ package com.saphal.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BlogServiceApplication {
 
 	public static void main(String[] args) {
---
+++ eureka/src/test/java/com/saphal/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saphal.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
