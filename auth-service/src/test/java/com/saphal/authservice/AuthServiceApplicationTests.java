ca350ee80a3a112e9dafd21bc2c9cab071cca029
saphalpathak <saphalpathak3@gmail.com>
2023-09-22 20:47:35 +0545

    Services Added     1. Auth Service     2. Blog Service

---
+++ auth-service/src/test/java/com/saphal/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saphal.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ blog-service/src/test/java/com/saphal/blogservice/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saphal.blogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
